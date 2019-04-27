dd220933e0f2cef1dbe236dffad093ae643b96d7
Praneesh Khanna <Praneesh.Khanna@ibm.com>
2019-04-27 18:40:35 -0400

    binary_search_iterative added

--- binary_serach.py
+++ binary_serach.py
@@ -1,27 +1,22 @@
-#binary_serach.py
+#binary_serach.py Iterative Version 
 
-def binary_search(input_array, value):
-    """Your code goes here."""
-    if len(input_array) % 2 == 0:
-        middle_element = input_array[len(input_array)/2 - 1]
-    else:
-        middle_element = input_array[len(input_array)/2]
-        
-    check_element = None
-    index_val = None
-    
-    while check_element!= value or array_index :
-        if value == middle_element:
-            print('Search Element found at index {}'.format(input_array.index(value)))
-            return 1
-            break
-        elif value > middle_element :
-            new_array = input_array[middle_element:]
-        elif value < middle_element:
-            new_array = input_array[:middle_element]
-        middle_element = new_array[len(new_array)/2]
-    
-    print('Value doesn\'t exist in the given list')
-    return -1
+def binary_search_iterative(input_array, value):
+    low_index = 0
+    high_index = len(input_array) - 1
+    while low_index <= high_index:
+        middle_index = (low_index + high_index)//2 # rounds off to lower middle element
+        if value == input_array[middle_index]:
+        	print('Search value {} found at index {}'.format(value, input_array.index(value)))
+        	return True
+        elif value < input_array[middle_index]:
+            high_index = middle_index - 1
+        else:
+            low_index = middle_index + 1
+  
+    print('Search value {} not found'.format(value))
+    return False
 
+
+
+binary_search_iterative(input_array=[1,2,3,4,5,7,8,9,12,22,34,36], value=6500)
 
