a9b473436534eaa30b7f27f704d5f2ca044e82c0
Praneesh Khanna <khanna.praneesh@gmail.com>
2019-04-28 20:51:23 -0400

    adding binary search

--- towersOfHanoi.py
+++ towersOfHanoi.py
@@ -1,1 +1,6 @@
-# Towers of Hanoi 
+# Assembling towers of Hanoi using recursion
+
+# Goal: Shift discs from one tower to another
+# Rules: There are 3 towers and m discs. Only a smaller disc can come over a disc.
+# n -> number of discs
+
