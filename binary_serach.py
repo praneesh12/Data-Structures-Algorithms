f4d0b1e9824d821ca642c099e2d8155b9e254db3
Praneesh Khanna <khanna.praneesh@gmail.com>
2019-05-01 09:17:20 -0400

    adding recursive binary

--- binary_serach.py
+++ binary_serach.py
@@ -1,19 +1,25 @@
 #binary_serach.py Iterative Version 
 
-def binary_search_iterative(input_array, value):
-    low_index = 0
-    high_index = len(input_array) - 1
-    while low_index <= high_index:
-        middle_index = (low_index + high_index)//2 # rounds off to lower middle element
-        if value == input_array[middle_index]:
-        	print('Search value {} found at index {}'.format(value, input_array.index(value)))
-        	return True
-        elif value < input_array[middle_index]:
-            high_index = middle_index - 1
-        else:
-            low_index = middle_index + 1
-  
-    print('Search value {} not found'.format(value))
-    return False
+import logging
 
-binary_search_iterative(input_array=[1,2,3,4,5,7,8,9,12,22,34,36], value=6500)
+logger = logging.getLogger(__name__)
+
+def binary_search_iterative(input_array, value):
+	try:
+	    low_index = 0
+	    high_index = len(input_array) - 1
+	    while low_index <= high_index:
+	        middle_index = (low_index + high_index)//2 # rounds off to lower middle element
+	        if value == input_array[middle_index]:
+	        	print('Search value {} found at index {}'.format(value, input_array.index(value)))
+	        	return True
+	        elif value < input_array[middle_index]:
+	            high_index = middle_index - 1
+	        else:
+	            low_index = middle_index + 1
+	except Exception as e:
+		logging.info('Error message: {}')
+		# print('Search value {} not found'.format(value))
+        # return False
+
+
