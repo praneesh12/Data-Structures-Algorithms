65acc6e3ba735aa43c87a5f24fb0c920df7fc705
Praneesh Khanna <khanna.praneesh@gmail.com>
2019-05-05 00:20:23 -0400

    test bubbleSort

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,11 +1,7 @@
 # Sorting Algorithm Naive approach : Bubble Sort
-# Runtime 
-# array = [132,143,156,169,172,34,44,55,56,76,87,98,172,1,3,4,8,12,122,188,234,122,132,143,156,169]
 
 #Create a randomized array of length 'length' and range 0,maxint
 from random import randint
-
-
 def create_random_array(length=10, maxint=50):
 	return [randint(0, maxint) for _ in range(length)]
 
