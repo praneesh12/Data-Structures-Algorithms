80b5b7cf86a630488d3d02c16f56889c50486e5c
Praneesh Khanna <khanna.praneesh@gmail.com>
2019-05-05 00:46:45 -0400

    adding selection sort variant

--- selectionSort.py
+++ selectionSort.py
@@ -6,6 +6,7 @@ from random import randint
 def create_random_array(length=10, maxint=50):
 	return [randint(0, maxint) for _ in range(length)]
 
+# Creates a new array
 def selectionSort(array):
 	return_array = []
 
@@ -16,6 +17,32 @@ def selectionSort(array):
 		array.remove(min_element)
 	return return_array
 
+# array = create_random_array()
+# print('Random array before sorting \n{}\n'.format(array))
+# print('Random array after sorting \n{}'.format(selectionSort(array)))
+
+# Selection sort without creating a new array
+def selectionSort_same(array):
+	suffixSt = 0
+
+	while suffixSt != len(array):
+		for i in range(suffixSt,len(array)):
+			if array[i] < array[suffixSt]:
+				array[suffixSt], array[i] = array[i], array[suffixSt]
+		suffixSt += 1
+	return array
+
 array = create_random_array()
 print('Random array before sorting \n{}\n'.format(array))
-print('Random array after sorting \n{}'.format(selectionSort(array)))
+print('Random array after sorting \n{}'.format(selectionSort_same(array)))
+
+
+
+
+
+
+
+
+
+
+
