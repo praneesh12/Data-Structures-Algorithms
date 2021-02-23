d62134fb41c5068f512f4a221954caadc583269d
praneesh12 <khanna.praneesh@gmail.com>
2021-02-23 08:24:22 -0500

    add binary tree ds

---
+++ DataStructures/BinaryTree/binary_tree.py
@@ -0,0 +1,39 @@
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.right = None
+        self.left = None
+
+
+class BinaryTreeNode(object):
+    def __init__(self, root):
+        self.root = Node(root)
+
+    def print_tree(self, traversal_type):
+        if traversal_type == "preorder":
+            return self.preorder_print(tree.root, "")
+        else:
+            print("Traversal type " + str(traversal_type) + " is not supported")
+
+    def preorder_print(self, start, traversal):
+        """ Root -> Left -> Right """
+        
+        if start:
+            traversal += (str(start.value) + "-")
+            traversal = self.preorder_print(start.left, traversal)
+            traversal = self.preorder_print(start.right, traversal)
+        return traversal
+
+
+
+
+tree = BinaryTreeNode(1)
+tree.root.left = Node(2)
+tree.root.right = Node(3)
+tree.root.left.left = Node(4)
+tree.root.left.right = Node(5)
+tree.root.right.left = Node(6)
+tree.root.right.right = Node(7)
+tree.root.right.right.right = Node(8)
+
+print(tree.print_tree("preorder"))
